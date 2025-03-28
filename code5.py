d298eb2ccc8d61b4ca6473b037c43b11a7277a61
.asm <irajmirzazadeh2024@gmail.com>
2025-03-28 21:29:10 +0330

    Create code5.py

---
+++ code5.py
@@ -0,0 +1,7 @@
+x = 5  # عدد صحیح (int)
+y = 2.5  # عدد اعشاری (float)
+
+result = x + y  # پایتون خودکار x را به float تبدیل می‌کند
+
+print(result)  # خروجی: 7.5
+print(type(result))  # خروجی: <class 'float'>
