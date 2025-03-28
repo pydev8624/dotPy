14b468eca028bee9c0bc296f59c5a10fa9735899
.asm <irajmirzazadeh2024@gmail.com>
2025-03-28 21:21:50 +0330

    Create code4.py

---
+++ code4.py
@@ -0,0 +1,16 @@
+a=1
+b=2.333
+c='C'
+d="Hello"
+e=(1>2)
+
+print(type(a))
+#<class 'int'>
+print(type(b))
+#<class 'float'>
+print(type(c))
+#<class 'str'>
+print(type(d))
+#<class 'str'>
+print(type(e))
+#<class 'bool'>
