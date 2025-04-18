9320725f3b682ce08baca410f6baba104cd6d162
.asm <irajmirzazadeh2024@gmail.com>
2025-04-18 18:38:33 +0330

    Create mod.py

---
+++ mod.py
@@ -0,0 +1,15 @@
+def chap():
+    print("hello")
+
+def add(a,b):
+    return a+b
+
+class father:
+    def __init__(self):
+        self.name=None
+        self.age=None
+    def __change(self):
+        self.name=""
+    def chap(self):
+        print(self.name,self.age)
+
