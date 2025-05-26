b14ba571ae242d5bd6c580c82a7d75e1e9fa5ba6
.asm <irajmirzazadeh2024@gmail.com>
2025-05-26 20:30:23 +0330

    Update code106.py

--- code106.py
+++ code106.py
@@ -1,6 +1,7 @@
 i=int(input("number : "))
-try:
-    for item in [-2,-1,0,1,2,3]:
+for item in [-2,-1,0,1,2,3]:
+    try:
         print(i/item)
-except ZeroDivisionError:
-    print("Infinite")
+    except ZeroDivisionError:
+        print("infinite")
+    
