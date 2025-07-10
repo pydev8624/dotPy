3927625d14a804566405d660c36d450e522d54c3
.asm <irajmirzazadeh2024@gmail.com>
2025-07-10 17:25:15 +0330

    Update code194.py

--- code194.py
+++ code194.py
@@ -1,8 +1,9 @@
 import tempfile
+import time
 
-with tempfile.NamedTemporaryFile(mode='w+t') as tmp:
+with tempfile.NamedTemporaryFile(mode='w+t',dir='fd\\') as tmp:
     print("Temporary filename:", tmp.name)
     tmp.write("This file has a name!")
     tmp.seek(0)
     print(tmp.read())
-
+    time.sleep(10)
