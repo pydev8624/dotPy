d57f859db5193ab0c3e5a461cf8ac6eae3afd0e4
.asm <irajmirzazadeh2024@gmail.com>
2025-07-22 22:31:28 +0330

    Create code211.py

---
+++ code211.py
@@ -0,0 +1,10 @@
+class person:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    def __str__(self):
+        return "Object created"
+    
+
+p=person(name='ali',age=30)
+print(p)
